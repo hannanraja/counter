39e862b28cc798da93549ec351755c5194ac990a
MuhammmadHannan <hannanraja1992@gmail.com>
2022-09-13 00:17:36 +0500

    Simple counter
    This is the simple counter app using classes in reactJS.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+class App extends React.Component {
+    constructor() {
+      super();
+      this.state = {count:0};
+    }
+    
+    increment = (count)=>{
+this.setState({count : this.state.count+1});
+    }
+    decrement = ()=>{
+      (this.state.count> 0 && this.setState({count :this.state.count-1}) )
+    
+          }
+          render() {
+            return (
+              <div
+              style={{marginTop: "20vh"}}>
+                 <h1>React Simple Counter App </h1>
+            
+              <div className='centeralign'>
+               
+                <button
+            onClick={this.decrement}
+            className="btn" >
+              -
+            </button>
+            <h2>Count : {this.state.count} </h2>
+            <button
+            onClick={this.increment}
+            className="btn" >
+              +
+            </button>
+            
+            </div>
+            </div>
+            )
+          }
+  }
+  export default App;
